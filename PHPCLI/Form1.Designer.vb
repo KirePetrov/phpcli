656581a3b831f08085536e7aae1fbe78d9976d9c
Kire Petrov <kire@petrov.mk>
2023-08-29 22:01:50 +0200

    init

---
+++ PHPCLI/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 66)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(440, 48)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PHP directory source"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(359, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Browse..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(347, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 120)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(440, 187)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Available PHP version(s)"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(428, 164)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 339)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(440, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Change PHP CLI version"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(12, 316)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(443, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "* Make sure that you don't have any running application(s) which is using PHP CLI" &
+    " right now!"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(440, 48)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Current version"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 21)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(428, 20)
+        Me.TextBox2.TabIndex = 0
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Select a folder"
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 383)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Change PHP CLI version"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ PHPCLI/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.IO
+Imports System.Security.Principal
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    '
+    Dim source As String = Nothing
+
+    Dim current As String = Nothing
+
+    Dim selected As String = Nothing
+
+    Dim regex As Regex = New Regex(".*\\php-\d+\.\d+\.\d+-.*")
+
+    Dim paths As Array = Strings.Split(System.Environment.GetEnvironmentVariable("PATH"), ";")
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Dim isAdmin As Boolean = New WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator)
+
+        If Not isAdmin Then
+            '
+            MessageBox.Show("Administrator privileges required to run this program!", "Run as administrator", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+            Environment.Exit(0)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '
+        For Each path As String In paths
+            ' Check if ends with regex
+            If regex.Match(path).Success Then
+                current = path
+                TextBox2.Text = current
+                Exit For
+            End If
+        Next
+
+        If current Is Nothing Then
+            ' Not found
+            MessageBox.Show("Current PHP version not found!", "Not found", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            ' Get parent directory
+            source = Path.GetDirectoryName(current)
+
+            refreshAvailablePHPVersions()
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        '
+        If ListBox1.SelectedItem IsNot Nothing Then
+            '
+            selected = source & "\" & ListBox1.SelectedItem
+
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '
+        FolderBrowserDialog1.SelectedPath = source
+
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            '
+            source = FolderBrowserDialog1.SelectedPath
+
+            refreshAvailablePHPVersions()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '
+        Dim cmd As Boolean = IsProcessRunning("cmd")
+
+        Dim shell As Boolean = IsProcessRunning("powershell")
+
+        If cmd OrElse shell Then
+            '
+            MessageBox.Show("Command Prompt or PowerShell process is running.", "Close applications", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            '
+            Dim newPaths As New List(Of String)()
+
+            For Each path As String In paths
+                If Not regex.Match(path).Success Then
+                    newPaths.Add(path)
+                End If
+            Next
+
+            newPaths.Add(selected)
+
+            Try
+                '
+                System.Environment.SetEnvironmentVariable("PATH", String.Join(";", newPaths), EnvironmentVariableTarget.Machine)
+
+                Dim result As DialogResult = MessageBox.Show("System Environment is updated successfully!", "Updated successfully", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                If result = DialogResult.OK Then
+                    Application.Exit()
+                End If
+            Catch ex As Exception
+                '
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Function IsProcessRunning(processName As String) As Boolean
+        '
+        Dim processes() As Process = Process.GetProcessesByName(processName)
+
+        Return processes.Length > 0
+    End Function
+
+    Private Sub refreshAvailablePHPVersions()
+        '
+        Dim folders() As String = Directory.GetDirectories(source)
+
+        TextBox1.Text = source
+
+        Button1.Enabled = False
+
+        ListBox1.Items.Clear()
+
+        ListBox1.ClearSelected()
+
+        For Each folder As String In folders
+            '
+            If regex.Match(folder).Success Then
+                '
+                ListBox1.Items.Add(Path.GetFileName(folder))
+            End If
+        Next
+    End Sub
+End Class
